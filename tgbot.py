f706d6f0ed4342df0bc465d7b5d2857819f5e382
Gorlov Andrey <a.b.gorlov@gmail.com>
2023-10-10 09:55:28 +0300

    test

---
+++ tgbot.py
@@ -0,0 +1,19 @@
+import telebot
+import emoji
+from telebot import types
+
+bot = telebot.TeleBot('6618497519:AAEHGb3mQGpvfj8EBLBIUy62_EFU49i2bME')
+def emojing(line):
+    return str(emoji.emojize(line))
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.from_user.id, emojing("Привет! :waving_hand: "))
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text.lower() in ('привет', 'привет!', emojing("Привет! :waving_hand: ")):
+        bot.send_message(message.from_user.id, emojing("Привет! :waving_hand: ")) 
+
+bot.polling(none_stop=True, interval=0)
